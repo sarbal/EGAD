e2f6fecfaf883355bc8a3f70f761b85c4f519661
Manthan Shah <manthan.ngu@gmail.com>
2018-01-24 11:40:19 -0500

    additional parameter added to function: plot and abline

--- R/plot_value_compare.R
+++ R/plot_value_compare.R
@@ -17,13 +17,13 @@
 #' @export
 #'
 plot_value_compare <- function(aucA, aucB, xlab = "AUROC", ylab = "AUROC", xlim = c(0, 1), ylim = c(0, 
-    1)) {
+    1), ...) {
     
     a <- mean(aucA, na.rm = TRUE)
     b <- mean(aucB, na.rm = TRUE)
     plot(aucA, aucB, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, bty = "n", cex.lab = 1.5, 
-        cex.axis = 1.2, pch = 19)
-    abline(0, 1, lwd = 3, col = "grey")
-    abline(v = a, lty = 2, col = 2)
-    abline(h = b, lty = 2, col = 2)
+        cex.axis = 1.2, pch = 19, ...)
+    abline(0, 1, lwd = 3, col = "grey", ...)
+    abline(v = a, lty = 2, col = 2, ...)
+    abline(h = b, lty = 2, col = 2, ...)
 } 
