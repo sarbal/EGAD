b55196604fcf7ae0961739246d7ee289ff0e8439
Manthan Shah <manthan.ngu@gmail.com>
2018-01-24 11:22:23 -0500

    additional parameter to plot functions

--- R/plot_densities.R
+++ R/plot_densities.R
@@ -23,7 +23,7 @@
 #'
 #' @export
 #'
-plot_densities <- function(hists, id, col = c("lightgrey"), xlab = "", ylab = "Density", mode = "hist") {
+plot_densities <- function(hists, id, col = c("lightgrey"), xlab = "", ylab = "Density", mode = "hist", ...) {
 
     if (mode == "hist") {
         ymax <- max(sapply(1:length(hists), function(i) max(hists[[i]][, 2])))
@@ -31,7 +31,7 @@ plot_densities <- function(hists, id, col = c("lightgrey"), xlab = "", ylab = "D
         
         
         plot(0, 0, col = 0, xlim = xrange, ylim = c(0, ymax), xlab = xlab, ylab = ylab, bty = "n", 
-            cex.lab = 1.5, cex.axis = 1.2, main = id)
+            cex.lab = 1.5, cex.axis = 1.2, main = id, ...)
         sapply(1:length(hists), function(i) polygon(hists[[i]], lwd = 0.5, col = make_transparent(col, 
             50)))
     } else {
@@ -40,7 +40,7 @@ plot_densities <- function(hists, id, col = c("lightgrey"), xlab = "", ylab = "D
         
         
         plot(0, 0, col = 0, xlim = xrange, ylim = c(0, ymax), xlab = xlab, ylab = ylab, bty = "n", 
-            cex.lab = 1.5, cex.axis = 1.2, main = id)
+            cex.lab = 1.5, cex.axis = 1.2, main = id, ...)
         sapply(1:length(hists), function(i) lines(hists[[i]], lwd = 2, col = make_transparent(col, 
             50)))
         
