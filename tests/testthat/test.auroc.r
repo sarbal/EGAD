5f3094452e5f931f21b6e0f771006704b96f5887
Manthan Shah <manthan.ngu@gmail.com>
2018-02-09 16:09:58 -0500

    Update test.auroc.r

--- tests/testthat/test.auroc.r
+++ tests/testthat/test.auroc.r
@@ -5,18 +5,18 @@ test_that("All AUROC calculations act as expected", {
   scores <- 1:11
   labels <- rep(0,11)
   labels[6] <- 1 
-  expect_equal( auroc_analytic(scores,labels), 0.5)  
+  expect_equal( auc_singlelist(scores,labels), 0.5)  
   roc <- get_roc(scores,labels)
   expect_equal( get_auc(roc[,1], roc[,2]) , 0.5)
 
   labels[1:5] <- 1
-  expect_equal( auroc_analytic(scores,labels), 0)
+  expect_equal( auc_singlelistc(scores,labels), 0)
   roc <- get_roc(scores,labels)
   expect_equal( get_auc(roc[,1], roc[,2]) , 0)
 
   labels[1:5] <- 0
   labels[6:11] <- 1
-  expect_equal( auroc_analytic(scores,labels), 1)
+  expect_equal( auc_singlelist(scores,labels), 1)
   roc <- get_roc(scores,labels)
   expect_equal( get_auc(roc[,1], roc[,2]) , 1)
 
